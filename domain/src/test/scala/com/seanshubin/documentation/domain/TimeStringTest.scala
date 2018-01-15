2e1af07b808409fa6d02776893db348aaace27be
Sean Shubin <sshubin@cj.com>
2018-01-15 09:25:05 -0800

    time string example

---
+++ domain/src/test/scala/com/seanshubin/documentation/domain/TimeStringTest.scala
@@ -0,0 +1,13 @@
+package com.seanshubin.documentation.domain
+
+import org.scalatest.FunSuite
+
+class TimeStringTest extends FunSuite {
+  test("string of times to time string") {
+    // given
+    val input = "12:32 34:01 15:23 9:27 55:22 25:56"
+    val expected = "2:32:41"
+    // when
+    // then
+  }
+}
