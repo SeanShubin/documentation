482674a24fdac4c8996e8a83e93450cce081e1d3
Sean Shubin <sshubin@cj.com>
2018-01-15 09:42:50 -0800

    renamed test

--- domain/src/test/scala/com/seanshubin/documentation/domain/TimeStringTest.scala
+++ domain/src/test/scala/com/seanshubin/documentation/domain/TimeStringTest.scala
@@ -3,7 +3,7 @@ package com.seanshubin.documentation.domain
 import org.scalatest.FunSuite
 
 class TimeStringTest extends FunSuite {
-  test("string of times to time string") {
+  test("string of times to sum as string") {
     // given
     val input = "12:32 34:01 15:23 9:27 55:22 25:56"
     val expected = "2:32:41"
